b240c1f260f4d49ebd670bd4d7fabe0a7d01d48f
Tolulope ogunleye <tjcode19@gmail.com>
2020-09-22 15:52:25 +0100

    2020-09-22

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,141 +1,136 @@
 <template>
   <div id="login-form">
     <v-container v-if="loading">
-    <div class="text-xs-center">
-      <v-progress-circular
-        indeterminate
-        :size="50"
-        :width="8"
-        color="green">
-      </v-progress-circular>
-    </div>
-  </v-container>
+      <div class="text-xs-center">
+        <v-progress-circular indeterminate :size="50" :width="8" color="green"></v-progress-circular>
+      </div>
+    </v-container>
     <div class="bg-red-200 border-l-4 border-red-500 text-red-700 p-4" v-show="error" role="alert">
       <p class="font-bold">Login Failed</p>
       <p>{{errorMsg}}</p>
     </div>
-  <form class="px-8 pt-6 pb-8 mb-4"  @submit.prevent="handleSubmit">
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Email
-      </label>
-      <input v-model="login.username" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
-    </div>
-    <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
-      </label>
-      <input v-model="login.password" class="shadow appearance-none border border-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-      <!-- <p class="text-red-500 text-xs italic">Please choose a password.</p> -->
-    </div>
-    <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-        Sign In
-      </button>
-      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
-      </a>
-    </div>
-     
-  </form>
-  <p class="text-center text-gray-500 text-xs">
-    &copy;2020 Mini CRM. tjcode19
-  </p>
-    
+    <form class="px-8 pt-6 pb-8 mb-4" @submit.prevent="handleSubmit">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Email</label>
+        <input
+          v-model="login.username"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username"
+          type="text"
+          placeholder="Username"
+        />
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+        <input
+          v-model="login.password"
+          class="shadow appearance-none border border-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          placeholder="******************"
+        />
+        <!-- <p class="text-red-500 text-xs italic">Please choose a password.</p> -->
+      </div>
+      <div class="flex items-center justify-between">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >Sign In</button>
+        <a
+          class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          href="#"
+        >Forgot Password?</a>
+      </div>
+    </form>
+    <p class="text-center text-gray-500 text-xs">&copy;2020 Mini CRM. tjcode19</p>
   </div>
-
-  
 </template>
 
 <script>
- import {HTTP} from '../config';
+import { HTTP } from "../config";
 
-  export default {
-    name: 'login-form',
-     props : ["nextUrl"],
-    data() {
-      return {
-        login: {
-          username: '',
-          password: ''       
-        },
-        error:false,
-        errorMsg:"",
-        loading:false        
+export default {
+  name: "login-form",
+  props: ["nextUrl"],
+  data() {
+    return {
+      login: {
+        username: "",
+        password: "",
+      },
+      error: false,
+      errorMsg: "",
+      loading: false,
+    };
+  },
+  methods: {
+    handleSubmit() {
+      // this.$emit('add:login', this.login)
+      if (this.login.username != "" && this.login.password != "") {
+        this.loading = true;
+        HTTP.post("auth/login", this.login).then((request) =>
+          this.loginSuccessful(request)
+        );
+        //.catch((e) => this.loginFailed(e))
+        console.log("testing handleSubmit");
+      } else {
+        this.error = true;
+        this.errorMsg = "Username and Password must be field";
       }
     },
-    methods: {
-    handleSubmit() {  
-       
-       // this.$emit('add:login', this.login)
-       if(this.login.username!= '' && this.login.password!=''){
-         this.loading= true 
-          HTTP
-          .post('auth/login', this.login)
-          .then(request => this.loginSuccessful(request))
-          //.catch((e) => this.loginFailed(e))
-          console.log('testing handleSubmit')
-       }
-       else{
-         this.error = true;
-         this.errorMsg = 'Username and Password must be field'
-       }
-         
-    },
 
-    loginSuccessful (req) {
-      console.log('Login Responded')
+    loginSuccessful(req) {
+      console.log("Login Responded");
       this.loading = false;
       if (!req.data.status) {
-        this.error = true
-        this.errorMsg = req.data.message        
-        this.loginFailed()
-        return
+        this.error = true;
+        this.errorMsg = req.data.message;
+        this.loginFailed();
+        return;
       }
 
       localStorage.token = req.data.token;
       localStorage.user = req.data.type;
       localStorage.userID = req.data.data.id;
-      localStorage.setItem('userCompany',JSON.stringify(req.data.company))
-      
-      console.log('UserID: '+localStorage.userID)
-      this.error = false;
-     let is_admin = (req.data.type =='admin')?true:false;
-     let is_company = (req.data.type =='company')?true:false;
-     let is_employee = (req.data.type =='employee')?true:false;
+      localStorage.setItem("userCompany", JSON.stringify(req.data.company));
 
-      if (localStorage.token != null){
-        this.$emit('loggedIn')
-        if(this.$route.params.nextUrl != null){
-            this.$router.push(this.$route.params.nextUrl)
+      console.log("UserID: " + localStorage.userID);
+      this.error = false;
+      let is_admin = req.data.type == "admin" ? true : false;
+      let is_company = req.data.type == "company" ? true : false;
+      let is_employee = req.data.type == "employee" ? true : false;
+
+      if (localStorage.token != null) {
+        this.$emit("loggedIn");
+        if (this.$route.params.nextUrl != null) {
+          this.$router.push(this.$route.params.nextUrl);
+        } else {
+          if (is_admin) {
+            this.$router.push("dashboard");
+          } else if (is_company) {
+            this.$router.push("dashboardCompany");
+          } else if (is_employee) {
+            this.$router.push("dashboardEmployee");
+          }
         }
-        else {
-            if(is_admin){
-                this.$router.push('dashboard')
-            }
-            else if(is_company){
-                this.$router.push('dashboardCompany')
-            }
-            else if(is_employee) {
-                this.$router.push('dashboardEmployee')
-            }
-        }
-    }
-      
+      }
     },
-    loginFailed () {
-       console.log('Login Failed, Msg: '+ this.errorMsg +"Error: "+ this.error)
-      delete localStorage.token
-    }
+    loginFailed() {
+      this.loading = false;
+      console.log(
+        "Login Failed, Msg: " + this.errorMsg + "Error: " + this.error
+      );
+      delete localStorage.token;
+    },
   },
-  }
+};
 </script>
 
 <style scoped>
-  form {
-    margin-bottom: 2rem;
-  }
-  .v-progress-circular{
-    margin: 1rem
-  }    
+form {
+  margin-bottom: 2rem;
+}
+.v-progress-circular {
+  margin: 1rem;
+}
 </style>
